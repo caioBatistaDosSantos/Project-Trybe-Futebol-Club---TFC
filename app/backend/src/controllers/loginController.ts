c6640c2916410fd7b9dd3f01157ec069adebcc3d
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-07-07 23:36:50 -0300

    biul: add lib http-status-codes

--- app/backend/src/controllers/loginController.ts
+++ app/backend/src/controllers/loginController.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
 import generateJWT from '../utils/generateJWT';
 import { IService } from '../interfaces/loginInterface';
 
@@ -9,13 +10,17 @@ export default class Controller {
 
   async create(req: Request, res: Response, next: NextFunction) {
     try {
-      console.log('chegou')
+      console.log(StatusCodes);
       const { email, password } = req.body;
+
+      if (!email || !password) {
+        return res.status(StatusCodes.BAD_REQUEST).json({ message: 'All fields must be filled' });
+      }
       const user = await this.service.login({ email, password });
 
       const token = generateJWT(user);
 
-      return res.status(201).json({ token });
+      return res.status(StatusCodes.CREATED).json({ token });
     } catch (error) {
       next(error);
     }
--- app/backend/src/tests/RouteLogin.test.ts
+++ app/backend/src/tests/RouteLogin.test.ts
@@ -2,6 +2,7 @@ import * as sinon from 'sinon';
 import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http');
+import { StatusCodes } from 'http-status-codes';
 
 import { app } from '../app';
 import UserModel from '../database/models/UserModel';
@@ -35,7 +36,7 @@ describe('Teste a rota POST "/login"', () => {
         email: 'admin@admin.com',
         password: 'secret_admin',
       });
-    expect(response.status).to.be.equal(201);
+    expect(response.status).to.be.equal(StatusCodes.CREATED);
     expect(response.body).to.be.eql({ token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJ1c2VybmFtZSI6IkFkbWluIiwicm9sZSI6ImFkbWluIiwiZW1haWwiOiJhZG1pbkBhZG1pbi5jb20iLCJwYXNzd29yZCI6IiQyYSQwOCR4aS5IeGsxY3pBTzBuWlIuLkIzOTN1MTBhRUQwUlExTjNQQUVYUTdIeHRMaktQRVpCdS5QVyJ9LCJpYXQiOjE2NTcyNDQ3ODEsImV4cCI6MTY1NzI1MTk4MX0.oQkGxt7fEtGVjDBjd2L6PL00t6Elzs3xTc92Y-XZOdM" })
   });
 
@@ -44,7 +45,7 @@ describe('Teste a rota POST "/login"', () => {
       .send({
         password: 'secret_admin',
       });
-    expect(response.status).to.be.equal(400);
+    expect(response.status).to.be.equal(StatusCodes.BAD_REQUEST);
     expect(response.body).to.be.eql({ message: "All fields must be filled" })
   });
 
@@ -53,7 +54,7 @@ describe('Teste a rota POST "/login"', () => {
       .send({
         email: 'admin@admin.com',
       });
-    expect(response.status).to.be.equal(400);
+    expect(response.status).to.be.equal(StatusCodes.BAD_REQUEST);
     expect(response.body).to.be.eql({ message: "All fields must be filled" })
   });
 });
