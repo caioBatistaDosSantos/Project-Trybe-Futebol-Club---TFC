59de0c20bb812ace90abe9f9875e09d6fee74c3a
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-07-13 18:21:30 -0300

    feat: add routes

---
+++ app/backend/src/routes/index.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import { loginFactory, teamFactory } from '../factory/index';
+import validateToken from '../middlewares/validateToken';
+
+const routes = Router();
+
+routes.get('/login/validate', validateToken, (req, res, next) => {
+  loginFactory().validateLogin(req, res, next);
+});
+
+routes.get('/teams', (req, res, next) => {
+  teamFactory().getAllTeams(req, res, next);
+});
+
+routes.get('/teams/:id', (req, res, next) => {
+  teamFactory().getTeamById(req, res, next);
+});
+
+routes.post('/login', (req, res, next) => {
+  loginFactory().login(req, res, next);
+});
