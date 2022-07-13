0790f8f550f7f1c8e6cf956bc947095cd084b672
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-07-13 17:21:07 -0300

    feat: add taemController in controllers

---
+++ app/backend/src/controllers/taemController.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { IService } from '../interfaces/teamInterface';
+
+export default class Controller {
+  constructor(private service: IService) {
+    this.service = service;
+  }
+
+  async getAllTeams(req: Request, res: Response, _next: NextFunction) {
+    const teams = await this.service.getAllTeams();
+
+    return res.status(StatusCodes.OK).json(teams);
+  }
+}
