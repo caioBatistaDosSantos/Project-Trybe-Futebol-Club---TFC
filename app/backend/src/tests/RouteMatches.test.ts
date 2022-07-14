3b751214055bbf4984c28c8dbc7a9eacce25119e
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-07-14 19:27:27 -0300

    test: new test in route GET "matches"

--- app/backend/src/tests/RouteMatches.test.ts
+++ app/backend/src/tests/RouteMatches.test.ts
@@ -13,38 +13,38 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-const MATCHES = [
-  {
-    id: 1,
-    homeTeam: 16,
-    homeTeamGoals: 1,
-    awayTeam: 8,
-    awayTeamGoals: 1,
-    inProgress: false,
-    teamHome: {
-      teamName: "São Paulo"
-    },
-    teamAway: {
-      teamName: "Grêmio"
-    }
-  },
-  {
-    id: 41,
-    homeTeam: 16,
-    homeTeamGoals: 2,
-    awayTeam: 9,
-    awayTeamGoals: 0,
-    inProgress: true,
-    teamHome: {
-      teamName: "São Paulo"
-    },
-    teamAway: {
-      teamName: "Internacional"
-    }
-  }
-]
-
 describe('Teste a rota GET "/matches"', () => {
+  const MATCHES = [
+    {
+      id: 1,
+      homeTeam: 16,
+      homeTeamGoals: 1,
+      awayTeam: 8,
+      awayTeamGoals: 1,
+      inProgress: false,
+      teamHome: {
+        teamName: "São Paulo"
+      },
+      teamAway: {
+        teamName: "Grêmio"
+      }
+    },
+    {
+      id: 41,
+      homeTeam: 16,
+      homeTeamGoals: 2,
+      awayTeam: 9,
+      awayTeamGoals: 0,
+      inProgress: true,
+      teamHome: {
+        teamName: "São Paulo"
+      },
+      teamAway: {
+        teamName: "Internacional"
+      }
+    }
+  ];
+
   before(() => {
     sinon
       .stub(MatchesModel, "findAll")
@@ -57,6 +57,104 @@ describe('Teste a rota GET "/matches"', () => {
 
   it('Quando o GET "/matches" acontece corretamente', async () => {
     const response = await chai.request(app).get('/matches');
+    expect(response.status).to.be.equal(StatusCodes.OK);
+    expect(response.body).to.be.eql(MATCHES)
+  });
+});
+
+describe('Teste a rota GET "/matches?inProgress=true"', () => {
+  const MATCHES = [
+    {
+      id: 1,
+      homeTeam: 16,
+      homeTeamGoals: 1,
+      awayTeam: 8,
+      awayTeamGoals: 1,
+      inProgress: true,
+      teamHome: {
+        teamName: "São Paulo"
+      },
+      teamAway: {
+        teamName: "Grêmio"
+      }
+    },
+    {
+      id: 41,
+      homeTeam: 16,
+      homeTeamGoals: 2,
+      awayTeam: 9,
+      awayTeamGoals: 0,
+      inProgress: true,
+      teamHome: {
+        teamName: "São Paulo"
+      },
+      teamAway: {
+        teamName: "Internacional"
+      }
+    }
+  ];
+
+  before(() => {
+    sinon
+      .stub(MatchesModel, "findAll")
+      .resolves(MATCHES as unknown as MatchesModel[]);
+  });
+
+  after(()=>{
+    (MatchesModel.findAll as sinon.SinonStub).restore();
+  });
+
+  it('Quando o GET "/matches?inProgress=true" acontece corretamente', async () => {
+    const response = await chai.request(app).get('/matches?inProgress=true');
+    expect(response.status).to.be.equal(StatusCodes.OK);
+    expect(response.body).to.be.eql(MATCHES)
+  });
+});
+
+describe('Teste a rota GET "/matches?inProgress=false"', () => {
+  const MATCHES = [
+    {
+      id: 1,
+      homeTeam: 16,
+      homeTeamGoals: 1,
+      awayTeam: 8,
+      awayTeamGoals: 1,
+      inProgress: false,
+      teamHome: {
+        teamName: "São Paulo"
+      },
+      teamAway: {
+        teamName: "Grêmio"
+      }
+    },
+    {
+      id: 41,
+      homeTeam: 16,
+      homeTeamGoals: 2,
+      awayTeam: 9,
+      awayTeamGoals: 0,
+      inProgress: false,
+      teamHome: {
+        teamName: "São Paulo"
+      },
+      teamAway: {
+        teamName: "Internacional"
+      }
+    }
+  ];
+
+  before(() => {
+    sinon
+      .stub(MatchesModel, "findAll")
+      .resolves(MATCHES as unknown as MatchesModel[]);
+  });
+
+  after(()=>{
+    (MatchesModel.findAll as sinon.SinonStub).restore();
+  });
+
+  it('Quando o GET "/matches?inProgress=false" acontece corretamente', async () => {
+    const response = await chai.request(app).get('/matches?inProgress=false');
     expect(response.status).to.be.equal(StatusCodes.OK);
     expect(response.body).to.be.eql(MATCHES)
   });
