8864db4e4b701fef1c6ad38bd35528470a389ec0
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-07-21 12:38:00 -0300

    feat: add route GET "/leaderboard" and "/leaderboard/home" in routes

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,5 +1,10 @@
 import { Router } from 'express';
-import { loginFactory, teamFactory, matchFactory } from '../factory/index';
+import {
+  loginFactory,
+  teamFactory,
+  matchFactory,
+  leaderboardFactory,
+} from '../factory/index';
 import validateToken from '../middlewares/validateToken';
 import validateCreateMatch from '../middlewares/validateCreateMatch';
 
@@ -19,6 +24,14 @@ routes.get('/teams/:id', (req, res, next) => {
 
 routes.get('/matches', (req, res, next) => {
   matchFactory().getAllMatches(req, res, next);
+});
+
+routes.get('/leaderboard', (req, res, next) => {
+  leaderboardFactory().getLeaderboardAll(req, res, next);
+});
+
+routes.get('/leaderboard/home', (req, res, next) => {
+  leaderboardFactory().getLeaderboardHome(req, res, next);
 });
 
 routes.post('/login', (req, res, next) => {
