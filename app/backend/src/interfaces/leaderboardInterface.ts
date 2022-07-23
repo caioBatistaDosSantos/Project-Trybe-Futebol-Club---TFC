a5788470fc607eb164ed390502a866b3d4c2ee44
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-07-23 15:58:42 -0300

    fix: rename method getLeaderboardHomeOrAway

--- app/backend/src/interfaces/leaderboardInterface.ts
+++ app/backend/src/interfaces/leaderboardInterface.ts
@@ -1,14 +1,14 @@
 export interface ILeaderboard {
-  name: string; // Time`: Nome do time;
-  totalPoints: number; // `P`: Total de Pontos;
-  totalGames: number; // `J`: Total de Jogos;
-  totalVictories: number; // `V`: Total de Vitórias;
-  totalDraws: number; // `E`: Total de Empates;
-  totalLosses: number; // `D`: Total de Derrotas;
-  goalsFavor: number; // `GP`: Gols marcados a favor;
-  goalsOwn: number; // `GC`: Gols sofridos;
-  goalsBalance: number; // `SG`: Saldo total de gols;
-  efficiency: number; // `%`: Aproveitamento do time.
+  name: string;
+  totalPoints: number;
+  totalGames: number;
+  totalVictories: number;
+  totalDraws: number;
+  totalLosses: number;
+  goalsFavor: number;
+  goalsOwn: number;
+  goalsBalance: number;
+  efficiency: number;
 }
 
 export interface IClassLeaderboard {
@@ -16,6 +16,6 @@ export interface IClassLeaderboard {
 }
 
 export interface IService {
-  leaderboardHome(): Promise<ILeaderboard[]>;
+  getLeaderboardHomeOrAway(typeLeaderboard: string): Promise<ILeaderboard[]>;
   leaderboardAll(): Promise<ILeaderboard[]>;
 }
